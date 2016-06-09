56174f8b239a8be695372b9bc5648ff0883d21af
Adriano Tanoue <adriano.tanoue@gmail.com>
2016-06-09 16:54:38 -0300

    Create SearchPage.java

---
+++ java-files/pages/examples/SearchPage.java
@@ -0,0 +1,26 @@
+package com.wipro.testing.training.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.stereotype.Component;
+
+public class SearchPage extends AbstractPage{
+	
+	@FindBy(id = "lst-ib")
+	private WebElement searchField;
+
+	public SearchPage() {
+		super();
+		PageFactory.initElements(getDriver(), this);
+	}
+	
+	public void typeSearchText(String searchText){
+		searchField.sendKeys(searchText);
+	}
+	
+	public void navigateToPage (){
+		getDriver().get("https://www.google.com");
+	}
+
+}
